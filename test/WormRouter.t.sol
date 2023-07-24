2a19505b13420473e7cd2c02bdf5b77d88e74ee4
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-07-24 15:00:13 +0000

    Test passes

--- test/WormRouter.t.sol
+++ test/WormRouter.t.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.13;
 import "../src/WormRouter.sol";
 
 import "wormhole-solidity-sdk/testing/WormholeRelayerTest.sol";
+import "wormhole-solidity-sdk/WormholeRelayerSDK.sol";
 
-contract HelloWormholeTest is WormholeRelayerBasicTest {
+contract WormRouterTest is WormholeRelayerBasicTest {
     event GreetingReceived(string greeting, uint16 senderChain, address sender);
 
     WormRouter wormRouter;
+    ERC20Mock public token;
+    uint256 constant GAS_LIMIT = 150_000;
 
     function setUpSource() public override {
+        token = createAndAttestToken(sourceChain);
     }
 
     function setUpTarget() public override {
@@ -18,6 +22,34 @@ contract HelloWormholeTest is WormholeRelayerBasicTest {
     }
 
     function testTransferTokens() public {
-        
+        uint256 amount = 19e17;
+        address recipient = 0x1234567890123456789012345678901234567890;
+
+        token.approve(address(tokenBridgeSource), amount);
+        uint64 sequence = tokenBridgeSource.transferTokens(address(token), amount, targetChain, toWormholeFormat(recipient), 0, 0);
+
+        VaaKey[] memory vaaKeys = new VaaKey[](1);
+        vaaKeys[0] = VaaKey({
+            chainId: sourceChain,
+            sequence: sequence,
+            emitterAddress: toWormholeFormat(address(tokenBridgeSource))
+        });
+
+        (uint256 value,) = relayerSource.quoteEVMDeliveryPrice(targetChain, 0, GAS_LIMIT);
+
+        relayerSource.sendVaasToEvm{value: value}(
+            targetChain, 
+            address(wormRouter), 
+            abi.encode(ITokenBridge.completeTransfer.selector, address(tokenBridgeTarget)),
+            0,
+            GAS_LIMIT,
+            vaaKeys
+        );
+
+        performDelivery();
+
+        vm.selectFork(targetFork);
+        address wormholeWrappedToken = tokenBridgeTarget.wrappedAsset(sourceChain, toWormholeFormat(address(token)));
+        assertEq(IERC20(wormholeWrappedToken).balanceOf(recipient), amount);
     }
 }
